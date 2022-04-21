eec20e850fd24273fa7785deb87f0ffa6d6e729a
Ishaan Maheshwari <63367019+Ishaan-Maheshwari@users.noreply.github.com>
2022-04-21 12:18:41 +0530

    Update IndianNavyController.php

--- indian_navy/Controller/IndianNavyController.php
+++ indian_navy/Controller/IndianNavyController.php
@@ -9,44 +9,47 @@ class IndianNavyController extends Controller
 {
     public function index(Request $request){
 
+        //extracting request parameters
         $org_id = $request->input('org_id');
-        $year = $request->input('year',-1);
+        $year = (string)$request->input('year',-1);
 
+        //Model object
         $navydb = new IndianNavy();
+        //Format for failed api response
         $response = ["status" => 'failed'];
 
         if($org_id != null){
-            $result = $navydb->get_data($org_id);
 
+            $result = $navydb->get_data($org_id);
+           
             //Response Format
             $response['status']='success';
-            $response['data'] = ['Total_Certificate'=>0, 'Total_Marksheet'=>0, 'Total_Awards'=>0, 'Years'=>''];
+            $response['data'] = ['Total_Certificate'=>0, 'Total_Marksheet'=>0, 'Total_Awards'=>0];
 
             foreach($result as $resultline){
-                if($resultline->_id == 'NVMST'){
-                    $response['data']['Total_Marksheet'] = $resultline->Count;
-                    $response['data']['Total_Awards'] += $resultline->Count;
-                    $response['data']['Years'] = $resultline->YEAR;
+                if($resultline->DOC_TYPE == 'NVMST'){
+                    $response['data']['Total_Marksheet'] = $resultline->aggregate;
+                    $response['data']['Total_Awards'] += $resultline->aggregate;
                 }
 
-                if($resultline->_id == 'NVCER'){
-                    $response['data']['Total_Certificate'] = $resultline->Count;
-                    $response['data']['Total_Awards'] += $resultline->Count;
+                if($resultline->DOC_TYPE == 'NVCER'){
+                    $response['data']['Total_Certificate'] = $resultline->aggregate;
+                    $response['data']['Total_Awards'] += $resultline->aggregate;
                 }
             }
 
-            if($year != -1 && $year != null){
+            //Total Awards yearwise
+            if($year != "-1" && $year != null){
                 $yearwise_result = $navydb->get_yearwise_data($org_id,$year);
-                $response['yearwise_data'] = ['Total_Certificate'=>0, 'Total_Marksheet'=>0, 'Total_Awards'=>0, 'Years'=>''];
+                $response['yearwise_data'] = ['Total_Certificate'=>0, 'Total_Marksheet'=>0, 'Total_Awards'=>0];
                 foreach($yearwise_result as $resultline){
-                    if($resultline->_id == 'NVMST'){
-                        $response['yearwise_data']['Total_Marksheet'] = $resultline->Count;
-                        $response['yearwise_data']['Total_Awards'] += $resultline->Count;
-                        $response['yearwise_data']['Years'] = $resultline->YEAR;
+                    if($resultline->DOC_TYPE == 'NVMST'){
+                        $response['yearwise_data']['Total_Marksheet'] = $resultline->aggregate;
+                        $response['yearwise_data']['Total_Awards'] += $resultline->aggregate;
                     }
-                    if($resultline->_id == 'NVCER'){
-                        $response['yearwise_data']['Total_Certificate'] = $resultline->Count;
-                        $response['yearwise_data']['Total_Awards'] += $resultline->Count;
+                    if($resultline->DOC_TYPE == 'NVCER'){
+                        $response['yearwise_data']['Total_Certificate'] = $resultline->aggregate;
+                        $response['yearwise_data']['Total_Awards'] += $resultline->aggregate;
                     }
                 }
             }
